e8f0143033a7819dbe446668ab2dda414313525c
Yahya Adi <adiyahya454@gmail.com>
2026-04-13 15:06:14 +0700

    Add feature add per data progress add data ui and model

--- lib/page/download_page.dart
+++ lib/page/download_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:signals/signals_flutter.dart';
+import 'package:yt_playlist_dl/model/download.dart';
 import 'package:yt_playlist_dl/util/download_to_storage.dart';
 import 'package:yt_playlist_dl/util/input_decoration.dart';
 import 'package:yt_playlist_dl/util/labeled_checkbox.dart';
@@ -25,40 +27,52 @@ class _DownloadPageState extends State<DownloadPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: ListView(padding: EdgeInsets.all(15),children: [
-        TextField(controller: urlController,decoration: buildInputDecoration(label: 'Video ID')),
-        SizedBox(height: 12,),
-        Row(
+      child: Padding(
+        padding: EdgeInsetsGeometry.all(12),
+        child: Column(
           children: [
-            Expanded(child: LabeledCheckbox(initialValue: playlistCheck,label: 'Playlist', onChanged: (value){
-              if (value != null) {
-                playlistCheck = value;
-               
+            TextField(controller: urlController,decoration: buildInputDecoration(label: 'Video ID')),
+            SizedBox(height: 12,),
+            Row(
+              children: [
+                Expanded(child: LabeledCheckbox(initialValue: audioOnlyCheck,label: 'Audio Only', onChanged: (value){if (value != null) {
+                    audioOnlyCheck = value;
+                  }} )),
+              ],
+            ),
+            SizedBox(height: 12,),
+            Watch((context) {
+              return NiceButton(isLoading: isLoading.value,text: 'Get', onPressed: ()async {
+              if (audioOnlyCheck) {
+                mode = 'audio';
+              } else {
+                mode = 'video';
               }
-            } )),
-            Expanded(child: LabeledCheckbox(initialValue: audioOnlyCheck,label: 'Audio Only', onChanged: (value){if (value != null) {
-                audioOnlyCheck = value;
-               
-              }} )),
+            await downloadToStorage(urlController.text, mode);
+            });
+            }),
+            SizedBox(height: 12,),
+            Watch((context) {
+              if (errorMessage.value != null) {
+                return Text(errorMessage.value!);
+              } 
+              return SizedBox.shrink();
+            }),
+            Expanded(child: 
+            Watch((context) {
+              if (downloadItem.value == null) {return SizedBox.shrink();}
+              return ListView.builder(itemCount: downloadItem.value!.nameData.length,itemBuilder: (context, index) {
+                final title = downloadItem.value!.nameData[index].name;
+                return Watch((context) {
+                  final progress = downloadItem.value!.nameData[index].progress.value;
+                  return ListTile(title: Text(title), subtitle: LinearProgressIndicator(value: progress, minHeight: 2,),);
+                });
+              });
+            })
+            )
           ],
         ),
-        SizedBox(height: 12,),
-        NiceButton(text: 'Get', onPressed: ()async{
-          if (playlistCheck == false && audioOnlyCheck == false) {
-            mode = 'single_video';
-          }
-          if (playlistCheck == false && audioOnlyCheck == true) {
-            mode = 'single_audio';
-          }
-          if (playlistCheck == true && audioOnlyCheck == false) {
-            mode = 'playlist_video';
-          }
-          if (playlistCheck == true && audioOnlyCheck == true) {
-            mode = 'playlist_audio';
-          }
-        await downloadToStorage(urlController.text, mode);
-        })
-      ],),
+      ),
     );
   }
 }
--- lib/util/download_to_storage.dart
+++ lib/util/download_to_storage.dart
@@ -1,29 +1,37 @@
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'package:yt_playlist_dl/model/download.dart';
+final dio = Dio();
+final baseUrl = 'http://127.0.0.1:8000';
 Future<void> downloadToStorage(String url, String mode) async {
-  final dio = Dio();
-  final baseUrl = 'http://127.0.0.1:8000';
+  try {
+  isLoading.value = true;
+  errorMessage.value = null;
   final response = await dio.get(
     "$baseUrl/download",
     queryParameters: {"url": url, 'mode': mode},
   );
-
-  final folder = response.data['folder'];
-  final fileNames = response.data["files"] as List;
-
-  for (final fileName in fileNames) {
-    final fileUrl = "$baseUrl/file/$folder/$fileName";
+  final data = response.data as Map<String, dynamic>;
+  downloadItem.value = DownloadItem.fromJson(data);
+  } catch (e) {
+    errorMessage.value = e.toString();
+  } finally {
+    isLoading.value = false;
+  }
+  if (downloadItem.value != null) {
+  for (final file in downloadItem.value!.nameData) {
+    final folder = downloadItem.value!.id;
+    final fileUrl = "$baseUrl/file/$folder/${file.name}";
   final dir = await getExternalStorageDirectory();
-  final savePath = "${dir!.path}/$fileName";
-
+  final savePath = "${dir!.path}/${file.name}";
   await dio.download(
     fileUrl,
     savePath,
     onReceiveProgress: (received, total) {
-      print("${(received / total * 100).toStringAsFixed(0)}%");
+      file.progress.value = received/total;
+      print(received/total);
     },
   );
-  print("Saved to: $savePath");
+  }
   }
 }
