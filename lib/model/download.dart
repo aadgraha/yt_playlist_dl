862b6088cdacf17b35f92faf19e667b2af4a32d8
Yahya Adi <adiyahya454@gmail.com>
2026-04-15 16:38:10 +0700

    clean up download item model

--- lib/model/download.dart
+++ lib/model/download.dart
@@ -1,32 +1,4 @@
 import 'package:signals/signals.dart';
 
-enum DownloadStatus { idle, loading, downloading, done, error }
-
-class NameData {
-  final String name;
-  final progress = signal(0.0);
-  NameData ({required this.name});
-}
-
-class DownloadItem {
-  final String id;
-  final List<NameData> nameData;
-
-  DownloadItem({
-    required this.id,
-    required this.nameData,
-  });
-
-   factory DownloadItem.fromJson(Map<String, dynamic> json) {
-    return DownloadItem(
-      id: json['folder']?.toString() ?? '',
-      nameData: (json['files'] as List<dynamic>? ?? [])
-          .map((e) => NameData(name: e.toString()))
-          .toList(),
-    );
-  }
-}
-
 final isLoading = signal(false);
-var downloadItem = signal<DownloadItem?>(null);
 final errorMessage = signal<String?>(null);
